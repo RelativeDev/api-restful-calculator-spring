b43fb40909cdd4855e34c50cdbb01f1762d83c6b
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2022-11-16 04:45:35 -0200

    att

--- src/main/java/com/example/apirestful/resources/MathResource.java
+++ src/main/java/com/example/apirestful/resources/MathResource.java
@@ -3,26 +3,54 @@ package com.example.apirestful.resources;
 import com.example.apirestful.converters.NumberConverter;
 import com.example.apirestful.exception.UnsuportedMathOperationException;
 import com.example.apirestful.math.SimpleMath;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.apirestful.model.Person;
+import com.example.apirestful.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/person")
 public class MathResource {
+
+    @Autowired
+    private PersonService service;
 
     private SimpleMath math = new SimpleMath();
 
-    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
-            throw new UnsuportedMathOperationException("Please set a numeric value!");
-        }
-        return math.sum(NumberConverter.covertToDouble(numberOne), NumberConverter.covertToDouble(numberTwo));
+    @RequestMapping(method= RequestMethod.GET)
+    public List<Person> findbyId() {
+        return service.findAll();
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findbyId(@PathVariable(value = "id") String id) throws Exception {
+        return service.findById(id);
+    }
+
+    @RequestMapping(method= RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) throws Exception {
+        return service.create(person);
+    }
+
+    @RequestMapping(method= RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) throws Exception {
+        return service.update(person);
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) throws Exception {
+        service.delete(id);
     }
 
 
-    @RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    /*@RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
     public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsuportedMathOperationException("Please set a numeric value!");
@@ -61,5 +89,5 @@ public class MathResource {
             throw new UnsuportedMathOperationException("Please set a numeric value!");
         }
         return math.squareRoot(NumberConverter.covertToDouble(number));
-    }
+    }*/
 }
